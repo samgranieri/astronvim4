043a0dc6c1edbc76fd54c23aecb1545c40232783
Sam Granieri, Jr <sgranieri@thecitybase.com>
2024-01-07 15:01:06 -0600

    Getting Solarized

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -1,4 +1,8 @@
 return {
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.colorscheme.neosolarized-nvim" },
+  { import = "astrocommunity.pack.lua" },
+
   -- Add community imports to the `lua/config/lazy.lua` file in your Neovim configuration folder (typically ~/.config/nvim)
   -- this guarantees that the community plugin specs are loaded before the user configuration
 
